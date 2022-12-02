f47b1e00fdb034a5a8fb5bce88d5d6dfa292f468
Sanja Pavasovic <pavasovic.sanja@gmail.com>
2022-12-02 12:32:28 +0100

    First outline of code

---
+++ rand_num.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Random_Number:
+
+    def __init__(self, minRange, maxRange, amount):
+        self.amount = int(amount)
+        self.minRange = int(minRange)
+        self.maxRange = int(maxRange)
+
+    def rand_num(self):
+        if self.minRange < 0:
+            print("minRange must be equal to or larger than 0.")
+        elif self.maxRange < self.minRange:
+            print("maxRange must be bigger than minRange.")
+        else:
+            random_list = list(np.random.randint(low = self.minRange, high = self.maxRange, size = self.amount))
+            print(random_list)
+
+mytest = Random_Number(1,10,5)
+mytest.rand_num()
